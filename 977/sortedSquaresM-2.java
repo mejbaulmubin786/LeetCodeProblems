51420a09c0ffcc489b4be3e6d955566d561d033f
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-12-12 12:23:45 +0600

    letcode problem 896 is solved with js

--- 977/sortedSquaresM-2.java
+++ 977/sortedSquaresM-2.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] array) {
         int[] newArray = new int[array.length];
-        int pointerLeft = 0;
-        int pointerRight = array.length - 1;
-
-        for (int i = array.length - 1; i >= 0; i--) {
-            int leftSquared = array[pointerLeft] * array[pointerLeft];
-            int rightSquared = array[pointerRight] * array[pointerRight];
-
-            if (leftSquared > rightSquared) {
-                newArray[i] = leftSquared;
-                pointerLeft++;
-            } else {
-                newArray[i] = rightSquared;
-                pointerRight--;
-            }
+        int pointerLeft =0;
+        int pointerRight = array.length-1;
+    for(int i=array.length-1; i>=0; i--){
+        int leftSquared = array[pointerLeft] * array[pointerLeft];
+        int rightSquared = array[pointerRight] * array[pointerRight];
+        if(leftSquared>rightSquared){
+            newArray[i]=leftSquared;
+            pointerLeft++;
+        } else{
+            newArray[i]=rightSquared;
+            pointerRight--;
         }
-
-        return newArray;
     }
+    return newArray;
+}
 }
